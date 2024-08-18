e2d2cebe8f02a641d327418017f36b7a6355eb91
Aditya-Singh-00 <aditya.25.singh.01@gmail.com>
2024-08-18 11:37:06 +0530

    remove: unused imports

--- composeApp/src/commonMain/kotlin/com/movieloom/app/presentation/common/MovieCard.kt
+++ composeApp/src/commonMain/kotlin/com/movieloom/app/presentation/common/MovieCard.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
